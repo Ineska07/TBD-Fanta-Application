9ccf3456c9a5cc1ddfa8a3dc79b0bcbd2eccdc21
Inés B <inesbiebrich@hotmail.com>
2021-12-04 12:38:28 -0700

    Prueba

--- FantaApp/frmMain.cs
+++ FantaApp/frmMain.cs
@@ -17,12 +17,12 @@ namespace FantaApp
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
       (
-          int nLeftRect,     // x-coordinate of upper-left corner
-          int nTopRect,      // y-coordinate of upper-left corner
-          int nRightRect,    // x-coordinate of lower-right corner
-          int nBottomRect,   // y-coordinate of lower-right corner
-          int nWidthEllipse, // height of ellipse
-          int nHeightEllipse // width of ellipse
+          int nLeftRect,     
+          int nTopRect,      
+          int nRightRect,    
+          int nBottomRect,   
+          int nWidthEllipse, 
+          int nHeightEllipse 
       );
 
         public frmMain()
