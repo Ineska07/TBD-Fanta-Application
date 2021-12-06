88d9e3f6f6de9b4d46573015ac419213b66b0ad9
Erick David Munguia German <erickdavid01@hotmail.com>
2021-12-06 12:45:15 -0700

    Lo de las ventas por si acaso

---
+++ FantaApp/frmVentasAdd.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace FantaApp
+{
+    partial class frmVentasAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblFantaAdd = new System.Windows.Forms.Label();
+            this.btnAñadir = new System.Windows.Forms.PictureBox();
+            this.btnLimpiar = new System.Windows.Forms.PictureBox();
+            this.btnSalir = new System.Windows.Forms.PictureBox();
+            this.lblErrorVacio = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.fANTA_BDDataSet = new FantaApp.FANTA_BDDataSet();
+            this.productoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.productoTableAdapter = new FantaApp.FANTA_BDDataSetTableAdapters.ProductoTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAñadir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLimpiar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSalir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fANTA_BDDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFantaAdd
+            // 
+            this.lblFantaAdd.AutoSize = true;
+            this.lblFantaAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFantaAdd.ForeColor = System.Drawing.Color.White;
+            this.lblFantaAdd.Location = new System.Drawing.Point(12, 9);
+            this.lblFantaAdd.Name = "lblFantaAdd";
+            this.lblFantaAdd.Size = new System.Drawing.Size(99, 16);
+            this.lblFantaAdd.TabIndex = 19;
+            this.lblFantaAdd.Text = "Añadir Orden";
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.BackColor = System.Drawing.Color.Transparent;
+            this.btnAñadir.BackgroundImage = global::FantaApp.Properties.Resources.btnGuardar;
+            this.btnAñadir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAñadir.InitialImage = null;
+            this.btnAñadir.Location = new System.Drawing.Point(99, 335);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(88, 32);
+            this.btnAñadir.TabIndex = 27;
+            this.btnAñadir.TabStop = false;
+            this.btnAñadir.LoadCompleted += new System.ComponentModel.AsyncCompletedEventHandler(this.btnAñadir_LoadCompleted);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.Transparent;
+            this.btnLimpiar.BackgroundImage = global::FantaApp.Properties.Resources.btnLimpiar;
+            this.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnLimpiar.InitialImage = null;
+            this.btnLimpiar.Location = new System.Drawing.Point(218, 335);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(51, 32);
+            this.btnLimpiar.TabIndex = 28;
+            this.btnLimpiar.TabStop = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Transparent;
+            this.btnSalir.BackgroundImage = global::FantaApp.Properties.Resources.btnExit;
+            this.btnSalir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSalir.InitialImage = null;
+            this.btnSalir.Location = new System.Drawing.Point(15, 337);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(64, 30);
+            this.btnSalir.TabIndex = 29;
+            this.btnSalir.TabStop = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // lblErrorVacio
+            // 
+            this.lblErrorVacio.AutoSize = true;
+            this.lblErrorVacio.ForeColor = System.Drawing.Color.Yellow;
+            this.lblErrorVacio.Location = new System.Drawing.Point(535, 11);
+            this.lblErrorVacio.Name = "lblErrorVacio";
+            this.lblErrorVacio.Size = new System.Drawing.Size(164, 13);
+            this.lblErrorVacio.TabIndex = 36;
+            this.lblErrorVacio.Text = "Uno o mas campos estan vacios!";
+            this.lblErrorVacio.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.DataSource = this.productoBindingSource;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(345, 264);
+            this.listBox1.TabIndex = 37;
+            // 
+            // fANTA_BDDataSet
+            // 
+            this.fANTA_BDDataSet.DataSetName = "FANTA_BDDataSet";
+            this.fANTA_BDDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productoBindingSource
+            // 
+            this.productoBindingSource.DataMember = "Producto";
+            this.productoBindingSource.DataSource = this.fANTA_BDDataSet;
+            // 
+            // productoTableAdapter
+            // 
+            this.productoTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmVentasAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(711, 386);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblErrorVacio);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.lblFantaAdd);
+            this.Name = "frmVentasAdd";
+            this.Text = "Añadir Orden";
+            this.Load += new System.EventHandler(this.frmVentasAdd_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.btnAñadir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLimpiar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnSalir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fANTA_BDDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFantaAdd;
+        private System.Windows.Forms.PictureBox btnAñadir;
+        private System.Windows.Forms.PictureBox btnLimpiar;
+        private System.Windows.Forms.PictureBox btnSalir;
+        private System.Windows.Forms.Label lblErrorVacio;
+        private System.Windows.Forms.ListBox listBox1;
+        private FANTA_BDDataSet fANTA_BDDataSet;
+        private System.Windows.Forms.BindingSource productoBindingSource;
+        private FANTA_BDDataSetTableAdapters.ProductoTableAdapter productoTableAdapter;
+    }
+}
---
+++ FantaApp/frmVentasAdd.cs
@@ -0,0 +1,97 @@
+﻿using FANTA;
+using System;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Windows.Forms;
+namespace FantaApp
+{
+    public partial class frmVentasAdd : Form
+    {
+        public frmVentasAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVentasAdd_Load(object sender, EventArgs e)
+        {
+            BD bd = new BD();
+        }
+
+        /*private void btnAñadir_Click(object sender, EventArgs e)
+        {
+            if (txtOrdenCantidad.Text == "" || txtOrdenDescuento.Text == "" || txtOrdenOrden.Text == "" || txtOrdenProducto.Text == "" || txtOrdenTipoPago.Text == "" || txtOrdenTotal.Text == "")
+            {
+                lblErrorVacio.Visible = true;
+            }
+            else
+            {
+                string cadenaFanta = "INSERT INTO Orden (Producto_ID, Nombre_Producto, Existencia__Producto, Tamaño_Envase, Precio__Producto, Proveedor_ID, Categoria_ID) VALUES" +
+                " (" + (Dgv.Rows.Count + 1).ToString() + ", '" + txtOrdenCantidad.Text + "', " + txtExistencia.Text + ", " + txtTamaño.Text + ", " + txtPrecio.Text + ", " + SacarProveedor().ToString() + ", " + SacarCategoria().ToString() + ")";
+                try
+                {
+                    SqlCommand insertFanta = new SqlCommand(cadenaFanta, BD.conectar());
+                    insertFanta.ExecuteNonQuery();
+                    BD.conectar().Close();
+                    BD bd = new BD();
+                    string consulta = "SELECT Nombre_Producto AS 'Nombre', Existencia__Producto AS 'Existencia', Tamaño_Envase AS 'Tamaño', " +
+                        "Precio__Producto AS 'Precio Unitario' FROM Producto";
+                    bd.VerificarConexion(Dgv, consulta);
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERROR, OCURRIO ALGO DURANTE EL INSERT");
+                }
+            }
+        }*/
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //limpiar();
+        }
+        /*private void limpiar()
+        {
+            txtOrdenCantidad.Text = "";
+            txtOrdenDescuento.Text = "";
+            txtOrdenOrden.Text = "";
+            txtOrdenProducto.Text = "";
+            txtOrdenTipoPago.Text = "";
+            txtOrdenTotal.Text = "";
+        }*/
+
+        public void ExportarDatos(DataGridView dataListado)
+        {
+            Microsoft.Office.Interop.Excel.Application exportarExcel = new Microsoft.Office.Interop.Excel.Application();
+
+            exportarExcel.Application.Workbooks.Add(true);
+
+            int indiceColumna = 0;
+            foreach (DataGridViewColumn columna in dataListado.Columns)
+            {
+                indiceColumna++;
+
+                exportarExcel.Cells[1, indiceColumna] = columna.Name;
+            }
+        }
+
+
+
+        private void btnAñadir_LoadCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            //oops
+        }
+
+        private void frmVentasAdd_Load_1(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'fANTA_BDDataSet.Producto' Puede moverla o quitarla según sea necesario.
+            this.productoTableAdapter.Fill(this.fANTA_BDDataSet.Producto);
+
+        }
+    }
+}
